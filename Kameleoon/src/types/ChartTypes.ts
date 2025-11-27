3ff6804a9a8b231226dc38a7335deabd75b258ea
Виктория Голубова <v.golubova@denexy.com>
2025-11-27 17:25:43 +0600

    Created ChartTypes.ts

---
+++ Kameleoon/src/types/ChartTypes.ts
@@ -0,0 +1,16 @@
+export default interface ChartInterface {
+    variations: Array<Variations>,
+    data: Array<Item>,
+}
+
+type Variations = { name: string } | { id: number, name: string };
+
+interface Item {
+    date: string,
+    visits: Visits,
+    conversions: Conversions,
+}
+
+type Visits = Record<string, number>;
+
+type Conversions = Record<string, number>;
