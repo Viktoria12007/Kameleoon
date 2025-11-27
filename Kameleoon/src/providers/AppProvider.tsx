6dce30d49128831b50737e280e91c7aea11d3549
Виктория Голубова <v.golubova@denexy.com>
2025-11-27 17:26:36 +0600

    Created AppProvider.tsx

---
+++ Kameleoon/src/providers/AppProvider.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import { AppContext } from "../contexts/AppContext.ts";
+import type AppState from "../types/AppStateTypes.ts";
+
+export function AppProvider({ children }) {
+    const [appState, setAppState] = useState<AppState | null>({
+        variation: { value: 1, label: "All variations selected" },
+        timePeriod: { value: 0, label: "Day"},
+        lineStyle: { value: 0, label: "Line style: line" },
+        theme: true,
+    });
+
+    return (
+        <AppContext.Provider value={{ appState, setAppState }}>
+            {children}
+        </AppContext.Provider>
+    );
+}
