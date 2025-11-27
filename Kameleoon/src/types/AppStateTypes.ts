6af746be1c7bb7504abad4972df290f91559d1b9
Виктория Голубова <v.golubova@denexy.com>
2025-11-27 17:25:26 +0600

    Created AppStateTypes.ts

---
+++ Kameleoon/src/types/AppStateTypes.ts
@@ -0,0 +1,6 @@
+export default interface AppState {
+    variation: { value: number, label: string },
+    timePeriod: { value: number, label: string },
+    lineStyle: { value: number, label: string },
+    theme: boolean,
+}
